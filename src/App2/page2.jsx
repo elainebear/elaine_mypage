17540051bafc09d5a86081cb4ee42c505d9164b1
Elaine <q7470251@gmail.com>
2025-03-10 14:01:13 +0800

    upload page2 and page3

--- src/App2/page2.jsx
+++ src/App2/page2.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import Page2 from "./App2";
 import Header from "../Header";
 import '../index.css'
+import {BrowserRouter, Routes} from "react-router-dom"
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
+    <BrowserRouter basename = "/elaine_mypage">
         <Header />
         <Page2 />
+    </BrowserRouter>
+        
     </React.StrictMode>
 );
