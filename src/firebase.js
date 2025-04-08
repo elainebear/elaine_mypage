d9571a4db2e99526b7f244d22ab7dbe3693de362
Elaine <q7470251@gmail.com>
2025-04-08 20:34:36 +0800

    Establish Firebase database connection for article page

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCO1IyXdQxGreYhaaIwjPYYiLjBNF-Hdf4",
-    authDomain: "personal-blog-c868e.firebaseapp.com",
-    projectId: "personal-blog-c868e",
-    storageBucket: "personal-blog-c868e.firebasestorage.app",
-    messagingSenderId: "459639967074",
-    appId: "1:459639967074:web:8f201c2cf75ea11d1d703b",
-    measurementId: "G-3KW4WH448R"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
