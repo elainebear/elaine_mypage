d6608703440488f6ad9a475707bc2899083eb376
Elaine <q7470251@gmail.com>
2025-04-10 23:40:39 +0800

    Implement sorting for articles and collections pages

--- src/pages/ArticleList.jsx
+++ src/pages/ArticleList.jsx
@@ -2,7 +2,7 @@
     return (
         <div className="articles-items">
             <h2><b>{article.title}</b></h2>
-            <p>發布日期：{article.date.toDate().toLocaleDateString()}</p>
+            <p>發布日期：{article.date.toLocaleDateString()}</p>
             <p>{article.abstr}</p>
         </div>
         
--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -12,47 +12,72 @@ import { collection, getDocs } from "firebase/firestore";
 function Articles() {
     const [articles, setArticles] = useState([]);
     const [query, setQuery] = useState("");
+    const [sortOrder, setSortOrder] = useState("newest");
+    //const []
 
+    // 從 firebase 抓資料
     useEffect(() => {
         async function fetchArticles() {
             const query_allarti = await getDocs(collection(db, "articles"));
             const data = query_allarti.docs.map((doc) => ({
                 id : doc.id,
                 ...doc.data(),
+                date: doc.data().date.toDate(), //確保 date 是 JS 日期物件
             }));
             setArticles(data);
         }
         fetchArticles();
     }, []);
-
-    const filterArticles = articles.filter(article => article.title.toLowerCase().includes(query.toLowerCase()));
-
     if (!articles) {
         return <p>Loading...</p>
     }
 
+    // 用搜尋框的文字即時搜尋 <input/>
+    const filterArticles = articles.filter(article => article.title.toLowerCase().includes(query.toLowerCase()));
     let heading = 'No article found';
     const count = query ? filterArticles.length : articles.length;
     
     if (count > 0) {
         const noun = count > 1 ? 'articles' : 'article';
-
         heading = query
             ? count + ' ' + noun + ' matching your search  '
             : count + ' ' + noun;
     } 
+
+    // 用 JS 內建sort 函式排列
+    const sortedArticles = [...filterArticles].sort((a, b) =>
+        sortOrder === "newest"
+        ? b.date - a.date
+        : a.date - b.date
+    );
+
+
     //---
     return (
         <section className="body-items">
             <h3 className="heading"> {heading} </h3>
-            <input
-                type="text"
-                value={query}
-                onChange={e => setQuery(e.target.value)}
-                placeholder="搜尋文章標題"
-            />
-            {filterArticles.length > 0 ? (
-                filterArticles.map((articles) => (
+            <div className="flex-container">
+                <input
+                    type="text"
+                    value={query}
+                    onChange={e => setQuery(e.target.value)}
+                    placeholder="搜尋文章標題"
+                />
+                {/*<button onClick={() => setSortOrder("newest")}>最新</button>*/}
+                {/*<button onClick={() => setSortOrder("oldest")}>最舊</button>*/}
+                <div className="select-wrapper">
+                    <select
+                        value={sortOrder}
+                        onChange={e => setSortOrder(e.target.value)}>
+                        <option value="newest">最新</option>
+                        <option value="oldest">最舊</option>
+                    </select>
+                </div>
+                
+            </div>
+            
+            {sortedArticles.length > 0 ? (
+                sortedArticles.map((articles) => (
                     <Link to={`/articles/${articles.id}`} key={articles.id}/*className="articles-items"*/>
                         <ArticleList article={articles} />
                     </Link>
