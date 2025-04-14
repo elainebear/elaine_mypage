9348f304ccd1093b83b7b34235a08b58de68eb77
Elaine <q7470251@gmail.com>
2025-04-14 22:22:05 +0800

    Updates

---
+++ assets/useArticles-BHN0QZaL.js
@@ -0,0 +1,1 @@
+import{r as a}from"./index-uB0efbGc.js";import{g as i,c as o,d as l}from"./firebase-Xm7XP1Pn.js";function u(){const[e,s]=a.useState([]);return a.useEffect(()=>{async function r(){const c=(await i(o(l,"articles"))).docs.map(t=>({id:t.id,...t.data(),date:t.data().date.toDate()}));s(c)}r()},[]),e}export{u as U};
