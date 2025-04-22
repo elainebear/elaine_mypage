a564029c1e20fd2ecb11f154589bf0078bcbcdb1
Elaine <q7470251@gmail.com>
2025-04-22 20:43:42 +0800

    Updates

--- assets/ArticleList-Dfiy2rsV.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-uB0efbGc.js";function i({article:t}){return s.jsxs("div",{className:"articles-items",children:[s.jsx("h2",{children:s.jsx("b",{children:t.title})}),s.jsxs("p",{children:["發布日期：",t.date.toLocaleDateString()]}),s.jsx("p",{children:t.abstr})]})}export{i as default};
